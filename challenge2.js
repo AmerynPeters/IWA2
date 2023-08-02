70111f2fef726c5cc700c41742b1e80d53d65358
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-08-02 16:41:20 +0200

    first commit

---
+++ challenge2.js
@@ -0,0 +1,10 @@
+//(c) ACME Inc. 2010 
+
+/** 
+ * This is the date that a user created their account
+ */
+
+const date = "10/07/2014";
+
+//This code is logging that "date" variable to the console
+console.log(date);
