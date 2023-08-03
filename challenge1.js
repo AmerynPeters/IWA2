bc77f0d082da918e46c91a2c4b0b78dc96997036
AmerynPeters <137108968+AmerynPeters@users.noreply.github.com>
2023-08-03 12:28:26 +0200

    added small changes after validation session

--- challenge1.js
+++ challenge1.js
@@ -10,4 +10,9 @@ const user = "John";
 
 const surname = "Smith";
 
+const array = ["john", "smith"]
+
+console.log(array[0], array[1])
+
+
 console.log(user, surname);
--- challenge3.js
+++ challenge3.js
@@ -5,8 +5,9 @@
 // - to impress investors
 
 
-console.warn(Security-scan-starting)
+console.warn('Security scan starting')
 
 //It is important to let user know when they can close the page 
 
 console.info('Please wait for scan to complete before closing the browser.')
+
